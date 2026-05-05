e94acff8cbeb9591092e2e0bd48182a6f8f14d59
Vincent Huang <vincent.huang@opennet.tw>
2026-05-06 00:35:23 +0800

    feat: add foundation enums (ControllerButton, ModifierKey, MouseButton, StickRole)

---
+++ Sources/FooTinderPad/Foundation/Types.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+enum ControllerButton: String, CaseIterable, Hashable {
+    case buttonA, buttonB, buttonX, buttonY
+    case leftShoulder, rightShoulder
+    case leftTrigger, rightTrigger
+    case leftThumbstickButton, rightThumbstickButton
+    case dpadUp, dpadDown, dpadLeft, dpadRight
+}
+
+enum MouseButton: String, Codable, Hashable {
+    case left, right, middle
+}
+
+enum StickRole: String, Codable {
+    case mouse, scroll, none
+}
+
+enum ModifierKey: Hashable, CaseIterable {
+    case leftCtrl, rightCtrl
+    case leftAlt, rightAlt
+    case leftShift, rightShift
+    case leftCmd, rightCmd
+    case fn
+}
